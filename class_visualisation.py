852809357f6cfad22e8dac5112e24dba00389f7c
Khawla Noman <khnoman@e1r3p9.1337.ma>
2026-06-14 12:16:20 +0100

    .

--- class_visualisation.py
+++ class_visualisation.py
@@ -8,6 +8,8 @@ class Visualisation:
         self.margin = 200
         self.min_x = 0
         self.min_y = 0
+        self.max_x = 0
+        self.max_y = 0
     
     def smallests_and_largest(self)-> tuple:
         if not self.zones:
@@ -20,6 +22,8 @@ class Visualisation:
         larg_y = max(element.y for  element in self.zones.values())
         self.min_x =  min_x
         self.min_y = min_y
+        self.max_x = larg_x
+        self.max_y = larg_y
         return  (min_x,min_y,larg_x,larg_y)
     
     def width_height(self, t_small_larg:tuple)-> tuple:
@@ -34,10 +38,18 @@ class Visualisation:
         return (window_width,window_hieght)
     
     def run_v(self,window, t_width_height):
-      
+        dict_zones={}
         pygame.init()
+        screen_info = pygame.display.Info()
+        width_screen =  screen_info.current_w
+        height_screen = screen_info.current_h
 
-        screen = pygame.display.set_mode((window[0],window[1]))
+        for key, element in self.zones.items():
+                dict_zones[element.name] = (element.x, element.y)
+        
+        scale_x = (width_screen - self.margin * 2) / (self.max_x - self.min_x)
+        scale_y = (height_screen - self.margin * 2) / (self.max_y - self.min_y)
+        screen = pygame.display.set_mode((width_screen,height_screen))
         pygame.display.set_caption("hello fly_in")
 
         running = True
@@ -47,15 +59,33 @@ class Visualisation:
                     return False
         
             screen.fill((255,255,255)) 
+
+            for value in self.connections:
+                for key,element in dict_zones.items():
+                        if key == value.name1:
+                                value1 = element
+                        if key == value.name2:
+                                value2 = element
+                                
+                print(f"{value.name1} -> {value.name2}: {value1} to {value2}")
+                con_v1_screan_x = ((value1[0] - self.min_x)* scale_x+ self.margin)
+                con_v1_screan_y = ((value1[1] - self.min_y)* scale_y+ self.margin)
+
+                con_v2_screan_x = ((value2[0] - self.min_x)* scale_x+ self.margin)
+                con_v2_screan_y = ((value2[1] - self.min_y)* scale_y+ self.margin)
+                pygame.draw.line(screen, pygame.Color("red"), (con_v1_screan_x,con_v1_screan_y), (con_v2_screan_x,con_v2_screan_y), width=2)
+
             for key, element in self.zones.items():
                 color = element.metadata.get("color", "white")
                 if color is None or color == None:
                     color = "white"
                 
-                screen_x = ((element.x - self.min_x) * self.scale + 25 )
-                screen_y = ((element.y - self.min_y) * self.scale + 25 )
-                pygame.draw.circle(screen, pygame.Color(color), (screen_x, screen_y), 30)
-
+                
+                screen_x = ((element.x - self.min_x) * scale_x + self.margin)
+                screen_y = ((element.y - self.min_y) * scale_y + self.margin)
+                pygame.draw.circle(screen, pygame.Color(color), (screen_x, screen_y), 50)
+            
+          
 
             pygame.display.update()
         # for key, element in self.zones.items():
